5626525f579397d5b2856849085fac176fed39a8
Mikel <mikelsanvi@gmail.com>
2024-04-30 10:34:46 +0200

    tests de getApartment

--- src/test/java/org/ies/tierno/building/model/BuildingTests.java
+++ src/test/java/org/ies/tierno/building/model/BuildingTests.java
@@ -1,40 +1,49 @@
 package org.ies.tierno.building.model;
 
+import org.ies.tierno.building.exceptions.ApartmentNotFoundException;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
 
 public class BuildingTests {
 
+    // Preparación
+    private Apartment apartment1B = new Apartment(1, "B",
+            List.of(
+                    new Owner("Bob", "Esponja")
+            )
+    );
+    private Apartment apartment2B =  new Apartment(2, "B",
+            List.of(
+                    new Owner("Peppa", "Pig")
+            )
+    );
+    private List<Apartment> apartments = List.of(
+            apartment1B,
+            apartment2B
+    );
+    private Building building = new Building(
+            "Gran Vía",
+            "Madrid",
+            28000,
+            apartments
+    );
+
     @Test
-    public void getApartmentTest() {
-        // Preparación
-        List<Apartment> apartments = List.of(
-                new Apartment(1, "B",
-                        List.of(
-                                new Owner("Bob", "Esponja")
-                        )
-                ),
-                new Apartment(2, "B",
-                        List.of(
-                                new Owner("Peppa", "Pig")
-                        )
-                )
-        );
-        Building building = new Building(
-                "Gran Vía",
-                "Madrid",
-                28000,
-                apartments
-        );
-
-        int floor = 2;
-        String door = "B";
+    public void getApartment2BTest() throws ApartmentNotFoundException {
         // Ejecutar el test
-
-
+        Apartment apartment = building.getApartment(2, "B");
 
         // Realizar comprobaciones (Assert)
-
+        Assert.assertEquals(apartment2B, apartment);
     }
+
+    @Test(expected = ApartmentNotFoundException.class)
+    public void getApartment3BTest() throws ApartmentNotFoundException {
+        // Ejecutar el test
+        building.getApartment(3, "B");
+    }
+
+
 }
